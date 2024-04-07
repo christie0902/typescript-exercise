c3b8bca0e1fba2626966634e5387cb9a4ed025dd
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-07 10:50:54 +0200

    union, nullable type

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,62 @@ function calculateTax (income:number, year=2022) : number {
 
 calculateTax(20000);
 
-let employee: {
-    readonly id:number,
-    // name?:string
-    name:string,
-    retire: (date: Date) => void
-} = {id:1, name:'',
+let employee: Employee  = {id:1, name:'',
 retire: (date: Date) => {console.log(date)}
 };
 
 employee.name = "A"
+
+type Employee = {
+    readonly id:number,
+    // name?:string
+    name:string,
+    retire: (date: Date) => void
+}
+
+function kgToLbs(weight: number | string) : number {
+ //Narrowing
+ if (typeof weight === 'number') {
+    //see all methods for numbers
+    return weight*2.2
+ } else {
+    //see all string methods
+    return parseInt(weight) * 2.2
+ }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+
+// intersection types
+type Draggable = {
+    drag: ()=>void
+};
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: ()=> {},
+    resize: ()=>{}
+}
+
+//literal type - can only be 50 or 100
+type Quantity = 50 | 100; 
+
+let quantity: Quantity = 100;
+
+type Measure = "cm" | "inch";
+//Nullable type
+function greet (name:string | null  |undefined) {
+    if (name)
+     console.log(name.toUpperCase());
+    else
+    console.log('Hola!');
+    
+}
+
+greet(null);
